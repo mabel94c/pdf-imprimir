dac6b333b044f731fc0190d23921e0719cf0fe40
Edgar Elio <edgarq354@gmail.com>
2024-02-19 22:35:38 -0400

    imprimir ticket

--- index.php
+++ index.php
@@ -8,19 +8,24 @@ $impresora1 = $_GET['impresora1'];
 $impresora2 = $_GET['impresora2'];
 $servidor1 = $_GET['servidor1'];
 $servidor2 = $_GET['servidor2'];
-$nombre=$id.".pdf";
+$nombre=$id."_ticket.pdf";
  
  if(isset($_GET['id_venta'])){
 	 // a route is created, (it must already be created in its repository(pdf)).
 	$ruta    = "ticket/".$nombre;
 
+	//remplazar ' ' por +
+	$ticket=str_replace(" ", "+", $ticket);
 	// decode base64
 	$pdf_b64 = base64_decode($ticket);
 
 	// you record the file in existing folder
 	if(file_put_contents($ruta, $pdf_b64)){
 	    //just to force download by the browser
-	   echo "Guardado correctamente";  
+	   echo "Guardado correctamente<br>";  
+	   echo $ruta."<br>";
+	   echo "PDF: ".$ticket;
+	  
 		if(isset($_GET['servidor1'])){
 		 exec("lpr -S ".$servidor1." -P ".$impresora1." ".$ruta);
 		}
